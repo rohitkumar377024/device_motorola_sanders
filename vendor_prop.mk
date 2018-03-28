d91628907e14d3524c5f3b2447d9789900553c02
Sean Tranchetti <stranche@codeaurora.org>
2018-03-28 09:29:04 -0600

    sanders: change data properties to comply with Treble

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -188,9 +188,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Netmgr
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.use_data_netmgrd=true \
+    ro.vendor.use_data_netmgrd=true \
     persist.data.netmgrd.qos.enable=true \
-    persist.data.mode=concurrent
+    persist.vendor.data.mode=concurrent
 
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
