c939ee0963030d1ac57fe86c9c725b589a1d4df7
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    Import Media and WFD Blobs from Daisy

--- device.mk
+++ device.mk
@@ -471,6 +471,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libaacwrapper \
+    libnl
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
@@ -481,4 +485,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
