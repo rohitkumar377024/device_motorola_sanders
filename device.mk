832625ffbbee669f44941f3d71756466a07f23ac
Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
2019-07-28 04:51:46 +0000

    Revert "sanders: Added DTV init Module"
    This reverts commit 150b83560b8deee0feaa46f6d017454c11e4e43b.

--- device.mk
+++ device.mk
@@ -369,12 +369,7 @@ PRODUCT_PACKAGES += \
     init.mmi.rc \
     init.mmi.usb.rc \
     init.qcom.rc \
-    init.qcom.sensors.sh \
-    init.sanders.rc \
-    init.spectrum.rc \
-    init.spectrum.sh \
-    init.dtv.rc \
-    init.dtv.sh
+    init.qcom.sensors.sh
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/etc/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
